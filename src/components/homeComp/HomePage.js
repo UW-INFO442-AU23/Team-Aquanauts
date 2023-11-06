a382aa4a3bc7c529c7e5fbacbe22324b863505ab
jianna-braza <jbraza@uw.edu>
2023-11-05 17:24:57 -0800

    css styling works on the homepage. font color and styling have been applied.

--- src/components/homeComp/HomePage.js
+++ src/components/homeComp/HomePage.js
@@ -10,9 +10,9 @@ export default function HomePage(props) {
       <main>
         <h2>What kind of Aquanaut are you?</h2>
         <div className="d-flex flex-column w-25 mx-auto">
-          <button type="button" className="btn btn-info btn-lg mb-5 mt-5"><NavLink to='/resources'>Resource page</NavLink></button>
-          <button type="button" className="btn btn-info btn-lg mb-5"><NavLink to='/donations'>Donation page</NavLink></button>
-          <button type="button" className="btn btn-info btn-lg mb-5"><NavLink to='/education'>Education page</NavLink></button>
+          <button type="button" className="btn btn-info btn-lg mb-5 mt-5 nav-link"><NavLink to='/resources'>Resource page</NavLink></button>
+          <button type="button" className="btn btn-info btn-lg mb-5 nav-link"><NavLink to='/donations'>Donation page</NavLink></button>
+          <button type="button" className="btn btn-info btn-lg mb-5 nav-link"><NavLink to='/education'>Education page</NavLink></button>
         </div>
       </main>
     </div>
