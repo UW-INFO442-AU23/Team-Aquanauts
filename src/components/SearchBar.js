f21efac3e832ee6a3414279702978c9d7f2340fe
jianna-braza <jbraza@uw.edu>
2023-11-08 11:28:45 -0800

    search bar errors displayed on screen rather than console. zipcode array has actual king county zip codes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import { useRef, useState } from "react";
-const ZIPCODES = ["10001", "10002", "10003"];
+const ZIPCODES = ["98001", "98002", "98003", "98004", "98005", "98006", "98007", "98008", "98009", "98010",
+  "98011", "98013", "98014", "98015", "98019", "98022", "98023", "98024", "98025", "98027", "98028", "98029",
+  "98030", "98031", "98032", "98033", "98034", "98035", "98038", "98039", "98040", "98041", "98042", "98045",
+  "98047", "98050", "98051", "98052", "98053", "98054", "98055", "98056", "98057", "98058", "98059", "98062",
+  "98063", "98064", "98065", "98068", "98070", "98071", "98072", "98073", "98074", "98075", "98083", "98092",
+  "98093", "98101", "98102", "98103", "98104", "98105", "98106", "98107", "98108", "98109", "98111", "98112",
+  "98114", "98115", "98116", "98117", "98118", "98119", "98121", "98122", "98124", "98125", "98126", "98131",
+  "98132", "98133", "98134", "98136", "98138", "98144", "98145", "98146", "98148", "98154", "98155", "98158",
+  "98160", "98161", "98164", "98166", "98168", "98171", "98174", "98177", "98178", "98188", "98198", "98199",
+  "98224", "98288"];
 
 export default function SearchBar(props) {
   const [query, setQuery] = useState('');
+  const [errorBool, setErrorBool] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   const handleChange = (e) => {
     setQuery(e.target.value);
@@ -17,10 +28,12 @@ export default function SearchBar(props) {
   // checks that query is correct length and formatted correctly
   function checkLengthAndFormat(queryString) {
     // checking if query is five or ten characters long
-    if (queryString.length == 5 || queryString.length == 10) { // it is 5 or 10 characters
+    if (queryString.length === 5 || queryString.length === 10) { // it is 5 or 10 characters
       //ten digit route
-      if (queryString.length == 10) {
-        if (queryString[5] != "-") {
+      if (queryString.length === 10) {
+        if (queryString[5] !== "-") {
+          setErrorBool(true);
+          setErrorMessage("query is not formatted correctly (hypen not present or in the wrong place)");
           console.log("query is not formatted correctly (hypen not present or in the wrong place)");
         } else {
           checkNumeric(queryString.substring(0, 5), queryString.substring(6, queryString.length));
@@ -30,6 +43,8 @@ export default function SearchBar(props) {
         checkNumeric(queryString, null);
       }
     } else { // not 5 or 10 digits long
+      setErrorBool(true);
+      setErrorMessage("query is not the right length");
       console.log("query is not the right length");
     }
   }
@@ -41,18 +56,22 @@ export default function SearchBar(props) {
 
   //check that all parts of the query are numerical
   function checkNumeric(firstHalf, secondHalf) {
-    if (secondHalf == null) { //five digit route
+    if (secondHalf === null) { //five digit route
       if (isNumeric(firstHalf)) { // just numbers
         checkZip(firstHalf);
       } else { //contains something other than numbers
+        setErrorBool(true);
+        setErrorMessage("query has characters other than numbers");
         console.log("query has characters other than numbers");
       }
     } else { //ten digit route
       if (isNumeric(firstHalf) && isNumeric(secondHalf)) { //both are just numbers
-        if (secondHalf.toString() != "0000") {
+        if (secondHalf.toString() !== "0000") {
           checkZip(firstHalf);
         }
       } else { //either has something other than a number
+        setErrorBool(true);
+        setErrorMessage("query has characters other than numbers");
         console.log("query has characters other than numbers");
       }
     }
@@ -61,9 +80,23 @@ export default function SearchBar(props) {
   //check that query is a valid zipcode
   function checkZip(zip) {
     if (ZIPCODES.includes(zip)) {
+      setErrorBool(false);
       console.log("zip is in array");
     } else {
+      setErrorBool(true);
+      setErrorMessage("zip is not in array");
       console.log("zip is not in array");
+    }
+  }
+
+  function DisplayError() {
+    if (errorBool) {
+      return (
+        <div>
+          <h2>Could not complete query</h2>
+          <p>{errorMessage}</p>
+        </div>
+      )
     }
   }
 
@@ -72,7 +105,8 @@ export default function SearchBar(props) {
   return (
     <div>
       <input className="text-specificity" id="text-input" placeholder="Enter a zipcode" value={query} type="search" onChange={handleChange} />
-      <div className="btn btn-white" id="apply-search" ref={ref2} onClick={handleSearch}>search!</div>
+      <div className="btn btn-primary" id="apply-search" ref={ref2} onClick={handleSearch}>search!</div>
+      <DisplayError />
     </div>
   )
 }
