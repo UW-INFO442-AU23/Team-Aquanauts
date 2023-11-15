fbad1f964df5548e59c27c2fca613ed6262c9b74
Joseph Sarsozo <js5602@uw.edu>
2023-11-14 22:56:47 -0800

    Added more images to education cards and info

--- src/components/educationComp/EducationPage.js
+++ src/components/educationComp/EducationPage.js
@@ -19,16 +19,19 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                    <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
+                    <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..." />
                     <h3 class="card-title">King County Water</h3>
                     <p class="card-text">Hover to learn more about where King County gets water!</p>
                   </div>
                 </div>
                 <div class="card-back bg-dark text-white">
                   <div class="card-body">
-                    <h3 class="card-title">Stats</h3>
-                    <p class="card-text">Suprise this one has more more more more content on the back!</p>
-                    <a href="#" class="btn btn-outline-secondary">Action</a>
+                    <h3 class="card-title">Facts</h3>
+                    <p class="card-text">King County gets most of it's water from watersheds.
+                                          click the button to head to Seattle Public Utilities on for
+                                          an in-depth look at the processes involved in getting clean water in central
+                                          King County.</p>
+                    <a href="https://www.seattle.gov/utilities/your-services/water/water-system" class="btn btn-outline-primary">Click me!</a>
                   </div>
                 </div>
               </div>
@@ -39,16 +42,18 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                  <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
-                    <h3 class="card-title">Front</h3>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                    <img src="..\img\water-lisa-fotio.jpg" class="card-img-top" alt="..." />
+                    <h3 class="card-title">Water Quality Issues</h3>
+                    <p class="card-text">Hover to learn more about common water quality problems in King County</p>
                   </div>
                 </div>
                 <div class="card-back bg-dark text-white">
                   <div class="card-body">
-                    <h3 class="card-title">Back #2</h3>
-                    <p class="card-text">Suprise this one has content on the back!</p>
-                    <a href="#" class="btn btn-outline-secondary">Action</a>
+                    <h3 class="card-title">Facts</h3>
+                    <p class="card-text">Despite King County having some of the cleanest water from it's watersheds,
+                                          there are still some common water quality problems within the county that.
+                                          Click the link button to read about some of the problems that plague the water in King County.</p>
+                    <a href="#" class="btn btn-outline-primary">Click Me!</a>
                   </div>
                 </div>
               </div>
@@ -59,14 +64,14 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                  <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
-                    <h3 class="card-title">Front</h3>
-                    <p class="card-text">With supporting text below as a natural lead-in to additional content. This one is a little because it has more text!</p>
+                    <img src="..\img\water-burst.jpg" class="card-img-top" alt="..." />
+                    <h3 class="card-title">King County Sanitation</h3>
+                    <p class="card-text">Hover to learn more about water sanitation in King County</p>
                   </div>
                 </div>
                 <div class="card-back bg-dark text-white">
-                  <div class="card-body text-primary">
-                    <h3 class="card-title">Wow! #3</h3>
+                  <div class="card-body text-white">
+                    <h3 class="card-title">Facts</h3>
                     <p class="card-text">Suprise this one has content on the back!</p>
                     <a href="#" class="btn btn-outline-primary">Action</a>
                   </div>
@@ -79,8 +84,8 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                  <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
-                    <h3 class="card-title">Front</h3>
+                    <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..." />
+                    <h3 class="card-title"></h3>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   </div>
                 </div>
@@ -88,7 +93,7 @@ export default function EducationPage(props) {
                   <div class="card-body">
                     <h3 class="card-title">Back</h3>
                     <p class="card-text">Suprise this one has more more more more content on the back!</p>
-                    <a href="#" class="btn btn-outline-secondary">Action</a>
+                    <a href="#" class="btn btn-outline-primary">Click Me!</a>
                   </div>
                 </div>
               </div>
@@ -99,7 +104,7 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                  <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
+                    <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..." />
                     <h3 class="card-title">Front</h3>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   </div>
@@ -108,7 +113,7 @@ export default function EducationPage(props) {
                   <div class="card-body">
                     <h3 class="card-title">Back</h3>
                     <p class="card-text">Suprise this one has more more more more content on the back!</p>
-                    <a href="#" class="btn btn-outline-secondary">Action</a>
+                    <a href="#" class="btn btn-outline-primary">Click Me!</a>
                   </div>
                 </div>
               </div>
@@ -119,7 +124,7 @@ export default function EducationPage(props) {
               <div class="card card-flip h-100">
                 <div class="card-front text-white bg-dark">
                   <div class="card-body">
-                  <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..."/>
+                    <img src="../img/harbor-activity.jpg" class="card-img-top" alt="..." />
                     <h3 class="card-title">Front</h3>
                     <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
                   </div>
@@ -128,7 +133,7 @@ export default function EducationPage(props) {
                   <div class="card-body">
                     <h3 class="card-title">Back</h3>
                     <p class="card-text">Suprise this one has more more more more content on the back!</p>
-                    <a href="#" class="btn btn-outline-secondary">Action</a>
+                    <a href="#" class="btn btn-outline-primary">Click Me!</a>
                   </div>
                 </div>
               </div>
