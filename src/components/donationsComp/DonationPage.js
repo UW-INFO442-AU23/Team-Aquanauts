ab3bd574f274b9d4118545b03cade0187416e9e9
jianna-braza <jbraza@uw.edu>
2023-11-27 10:59:14 -0800

    drop down filter working on the donations page

--- src/components/donationsComp/DonationPage.js
+++ src/components/donationsComp/DonationPage.js
@@ -3,21 +3,37 @@ import NavBar from '../NavBar.js';
 import DropSearch from './DropSearch.js';
 import { charityContent } from "../../data/waterCharities.js";
 import { CardList } from '../CardList.js';
+import { useRef, useState } from "react";
 let temp = 0;
 
 export default function DonationPage(props) {
+  const [focus, setFocus] = useState('');
 
   const cards = charityContent.map(item => {
     temp = temp + 1;
-    return (
-      <CardList
-        title={item.Name}
-        field1={item['Mission Statement']}
-        field2={item.Focus}
-        field3={item.Website}
-        key={temp}
-      />
-    )
+    if (focus === '') { //default
+      return (
+        <CardList
+          title={item.Name}
+          field1={item['Mission Statement']}
+          field2={item.Focus}
+          field3={item.Website}
+          key={temp}
+        />
+      )
+    } else { //group by focus
+      if (item.Focus === focus) {
+        return (
+          <CardList
+            title={item.Name}
+            field1={item['Mission Statement']}
+            field2={item.Focus}
+            field3={item.Website}
+            key={temp}
+          />
+        )
+      }
+    }
   })
 
   return (
@@ -27,7 +43,7 @@ export default function DonationPage(props) {
         <h1>Donation Page</h1>
       </header>
       <main>
-        <DropSearch />
+        <DropSearch setter={setFocus} />
 
         <div className="card-list">
           {cards}
--- src/components/donationsComp/DropSearch.js
+++ src/components/donationsComp/DropSearch.js
@@ -1,15 +1,22 @@
 import React from 'react';
 
 export default function DropSearch(props) {
+
+  function update(event) {
+    props.setter(event.target.textContent);
+  }
+
   return (
     <div className="dropdown">
       <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-        Dropdown button
+        Filter by Focus
       </button>
       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><a className="dropdown-item" href="#">Action</a></li>
-        <li><a className="dropdown-item" href="#">Another action</a></li>
-        <li><a className="dropdown-item" href="#">Something else here</a></li>
+        <li><button className="dropdown-item" onClick={update} >Wastewater</button></li>
+        <li><button className="dropdown-item" onClick={update} >Water Quality</button></li>
+        <li><button className="dropdown-item" onClick={update} >Water Access</button></li>
+        <li><button className="dropdown-item" onClick={update} >Affordability</button></li>
+        <li><button className="dropdown-item" onClick={update} >Infrastructure</button></li>
       </ul>
     </div>
   )
