c3f2da22f01e7360e990d04fd62c891afe75aadc
jianna-braza <jbraza@uw.edu>
2023-11-27 11:53:00 -0800

    filter drop down menu for charity focuses and beneficiaries

--- src/components/donationsComp/DonationPage.js
+++ src/components/donationsComp/DonationPage.js
@@ -3,15 +3,16 @@ import NavBar from '../NavBar.js';
 import DropSearch from './DropSearch.js';
 import { charityContent } from "../../data/waterCharities.js";
 import { CardList } from '../CardList.js';
-import { useRef, useState } from "react";
+import { useState } from "react";
 let temp = 0;
 
 export default function DonationPage(props) {
   const [focus, setFocus] = useState('');
+  const [beneficiaries, setBeneficiaries] = useState('');
 
   const cards = charityContent.map(item => {
     temp = temp + 1;
-    if (focus === '') { //default
+    if (focus === '' && beneficiaries === '') { //default
       return (
         <CardList
           title={item.Name}
@@ -21,17 +22,45 @@ export default function DonationPage(props) {
           key={temp}
         />
       )
-    } else { //group by focus
-      if (item.Focus === focus) {
-        return (
-          <CardList
-            title={item.Name}
-            field1={item['Mission Statement']}
-            field2={item.Focus}
-            field3={item.Website}
-            key={temp}
-          />
-        )
+    } else { //not default
+      if (focus !== '' && beneficiaries !== '') { //filter by both
+        if (item.Focus === focus && item.Beneficiary === beneficiaries) {
+          return (
+            <CardList
+              title={item.Name}
+              field1={item['Mission Statement']}
+              field2={item.Focus}
+              field3={item.Website}
+              key={temp}
+            />
+          )
+        }
+      } else if (focus === '') { //filter beneficiaries only
+        if (item.Beneficiary === beneficiaries) { //filter by both
+          return (
+            <CardList
+              title={item.Name}
+              field1={item['Mission Statement']}
+              field2={item.Focus}
+              field3={item.Website}
+              key={temp}
+            />
+          )
+        }
+      } else { //filter focuses only
+        if (item.Focus === focus) { //filter by both
+          if (item.Focus === focus && item.Beneficiary === beneficiaries) {
+            return (
+              <CardList
+                title={item.Name}
+                field1={item['Mission Statement']}
+                field2={item.Focus}
+                field3={item.Website}
+                key={temp}
+              />
+            )
+          }
+        }
       }
     }
   })
@@ -43,7 +72,8 @@ export default function DonationPage(props) {
         <h1>Donation Page</h1>
       </header>
       <main>
-        <DropSearch setter={setFocus} />
+        <DropSearch setFocus={setFocus} type='focuses' />
+        <DropSearch setBeneficiaries={setBeneficiaries} type='beneficiaries' />
 
         <div className="card-list">
           {cards}
--- src/components/donationsComp/DropSearch.js
+++ src/components/donationsComp/DropSearch.js
@@ -1,22 +1,48 @@
 import React from 'react';
+const focuses = ['Wastewater', 'Water Quality', 'Water Access', 'Affordability', 'Infrastructure', 'Law & Policy', 'Technology Adv', 'Hygiene', 'Public Health'];
+const beneficiaries = ['Households', 'Implementing Organizations', 'Impacted Communities', 'Local & State Government', 'Unhoused Communities', 'Environment', 'Utilities', 'Schools', 'General Public', 'Federal Government', ''];
+let temp = 100000;
 
 export default function DropSearch(props) {
+  let filterMessage = '';
+  if (props.type === "focuses") {
+    filterMessage = 'Filter by Focus';
+  } else {
+    filterMessage = 'Filter by Beneficiary';
+  }
 
   function update(event) {
-    props.setter(event.target.textContent);
+    if (props.type === "focuses") {
+      props.setFocus(event.target.textContent);
+    } else {
+      props.setBeneficiaries(event.target.textContent);
+    }
+  }
+
+  let options = [];
+  if (props.type === 'focuses') {
+    options = focuses.map(item => {
+      temp = temp + 1;
+      return (
+        <li><button key={temp} className="dropdown-item" onClick={update} >{item}</button></li>
+      );
+    })
+  } else {
+    options = beneficiaries.map(item => {
+      temp = temp + 100
+      return (
+        <li><button key={temp} className="dropdown-item" onClick={update} >{item}</button></li>
+      );
+    })
   }
 
   return (
     <div className="dropdown">
       <button className="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" >
-        Filter by Focus
+        {filterMessage}
       </button>
       <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-        <li><button className="dropdown-item" onClick={update} >Wastewater</button></li>
-        <li><button className="dropdown-item" onClick={update} >Water Quality</button></li>
-        <li><button className="dropdown-item" onClick={update} >Water Access</button></li>
-        <li><button className="dropdown-item" onClick={update} >Affordability</button></li>
-        <li><button className="dropdown-item" onClick={update} >Infrastructure</button></li>
+        {options}
       </ul>
     </div>
   )
